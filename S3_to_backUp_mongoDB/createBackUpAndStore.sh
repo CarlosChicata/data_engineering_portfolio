63c7a1f2619de50dfe4ebdf5f5d31fd9b213441a
CarlosChicata <carlos.fernando.chicata@gmail.com>
2021-02-24 11:50:28 -0500

    feat: create script to back up a mongo and to zip it

--- S3_to_backUp_mongoDB/createBackUpAndStore.sh
+++ S3_to_backUp_mongoDB/createBackUpAndStore.sh
@@ -1,3 +1,19 @@
-#!/bin/sh
-file_path=`pwd`"/Readme.md"
-tar -cvzf 'ziping.tar.gz' $file_path
+#!/bin/bash
+# this script create a back up of mongoDB and zipped its.
+
+echo 'Welcome! i will create a back Up of mongoDB and zip it. '
+
+current_path=`pwd`
+backup_path=$current_path'/backupMongo'
+destination_path='backupTestingDB.tar.gz'
+
+if [ ! -d $backup_path ]; then
+    mkdir 'backupMongo'
+fi
+
+echo '==============> 1/2 : generate backup'
+mongodump --host localhost --port 27017 --db chazkiv2 --gzip --out=$backup_path
+echo '==============> 2/2 : zippping backup'
+tar -cvzf $destination_path  $backup_path
+echo '==============> FINISH PROCCESS'
+exit 1
